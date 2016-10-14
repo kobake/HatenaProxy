529b6d1ffa7560ea36432d25952fb1446c0f99b3
kobake <kobake@users.sourceforge.net>
2016-10-14 16:16:25 +0900

    ページ体裁調整 (使わないけど)

--- HatenaProxy/Controllers/HomeController.cs
+++ HatenaProxy/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace HatenaProxy.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
