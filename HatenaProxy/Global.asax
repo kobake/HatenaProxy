9524cec34c63ebb7363cd4fdf1911771c26dc14f
kobake <kobake@users.sourceforge.net>
2016-10-14 16:02:13 +0900

    Add project files.

---
+++ HatenaProxy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HatenaProxy.MvcApplication" Language="C#" %>
---
+++ HatenaProxy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HatenaProxy.Startup))]
+namespace HatenaProxy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
